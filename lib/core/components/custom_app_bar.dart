1d3c272c01bcbdff7d5dd748758c00a486cb5e7b
egemenongel <egemenongel@hotmail.com>
2022-08-01 13:58:47 +0300

    Create Shop UI

---
+++ lib/core/components/custom_app_bar.dart
@@ -0,0 +1,150 @@
+import 'package:ecommerce_app/core/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const CustomAppBar({Key? key})
+      : preferredSize = const Size.fromHeight(kToolbarHeight * 2),
+        super(key: key);
+  @override
+  final Size preferredSize;
+  @override
+  CustomAppBarState createState() => CustomAppBarState();
+}
+
+class CustomAppBarState extends State<CustomAppBar> {
+  int _selectedIndex = 0;
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 5,
+      child: Column(
+        children: [
+          _buildHeader(context),
+          _buildActions(),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildHeader(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: context.colors.onPrimary,
+      iconTheme: IconThemeData(color: context.colors.primary),
+      titleTextStyle:
+          context.textTheme.headline5!.copyWith(color: context.colors.primary),
+      title: const Center(
+        child: Text(
+          'Products',
+        ),
+      ),
+      actions: [
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+            ))
+      ],
+    );
+  }
+
+  Row _buildActions() {
+    List<String> actionsList = [
+      'Popular',
+      'Newest',
+      'Reviews',
+      'Price: High to Low',
+      'Price: Low to High'
+    ];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        const SizedBox(
+          width: 10,
+        ),
+        const Icon(Icons.sort),
+        Text(actionsList[_selectedIndex]),
+        const Spacer(),
+        IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Text(
+                            'Sort by',
+                            style: context.textTheme.headline5!.copyWith(),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Expanded(
+                            child: GestureDetector(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: actionsList
+                                    .map((action) => GestureDetector(
+                                          onTap: () {
+                                            _onTap(actionsList.indexOf(action));
+                                            Navigator.pop(context);
+                                          },
+                                          child: Row(
+                                            children: [
+                                              Expanded(
+                                                child: Container(
+                                                  padding: context
+                                                      .paddingLowHorizontal,
+                                                  height: 50,
+                                                  color: actionsList.indexOf(
+                                                              action) ==
+                                                          _selectedIndex
+                                                      ? Colors.red
+                                                      : Colors.white,
+                                                  child: Align(
+                                                    alignment:
+                                                        Alignment.centerLeft,
+                                                    child: Text(
+                                                      action,
+                                                      style: context
+                                                          .textTheme.bodyText1!
+                                                          .copyWith(
+                                                        color: actionsList
+                                                                    .indexOf(
+                                                                        action) ==
+                                                                _selectedIndex
+                                                            ? Colors.white
+                                                            : Colors.black,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ))
+                                    .toList(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
+            icon: const Icon(Icons.list))
+      ],
+    );
+  }
+}
--- lib/features/shop/shop_view.dart
+++ lib/features/shop/shop_view.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app/core/components/custom_app_bar.dart';
+import 'package:ecommerce_app/core/extensions/context_extension.dart';
 import 'package:flutter/material.dart';
 
 class ShopView extends StatelessWidget {
@@ -5,6 +7,51 @@ class ShopView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Shop'));
+    return Scaffold(
+      appBar:const CustomAppBar(),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return _buildProduct(context);
+        },
+        itemCount: 30,
+      ),
+    );
   }
+
+  Stack _buildProduct(BuildContext context) => Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 150,
+                  child: ClipRRect(
+                    clipBehavior: Clip.antiAlias,
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                    child: Image.asset('assets/images/image_1.png'),
+                  ),
+                ),
+                const Text('Gucci'),
+                const Text('T-Shirt')
+              ],
+            ),
+          ),
+          Align(
+              alignment: const Alignment(0.8, 0.65),
+              child: Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: context.colors.onPrimary,
+                ),
+                child: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.favorite_border)),
+              ))
+        ],
+      );
 }
